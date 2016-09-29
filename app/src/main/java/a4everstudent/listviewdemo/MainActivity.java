9553e27f250a2503a4e6554354454c8ae08d1d0e
4EverStudent <mail@4everstudent.com>
2016-09-29 18:25:52 +0100

    Initial commit. Add ListView and items.

---
+++ app/src/main/java/a4everstudent/listviewdemo/MainActivity.java
@@ -0,0 +1,30 @@
+package a4everstudent.listviewdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView todoView = (ListView) findViewById(R.id.toDoListView);
+
+        ArrayList<String> toDo= new ArrayList<String>();
+        toDo.add("feed pets");
+        toDo.add("complete lecture 64");
+        toDo.add("finnish section 5");
+
+        ArrayAdapter<String>arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, toDo);
+
+        todoView.setAdapter(arrayAdapter);
+
+
+    }
+}
