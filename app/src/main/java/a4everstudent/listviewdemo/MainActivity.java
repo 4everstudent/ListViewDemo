6dcc86fc86ddef95d3230343b6ffda4e1693e550
4EverStudent <mail@4everstudent.com>
2016-10-01 16:16:09 +0100

    Add Toast onItemClick.

--- app/src/main/java/a4everstudent/listviewdemo/MainActivity.java
+++ app/src/main/java/a4everstudent/listviewdemo/MainActivity.java
@@ -1,12 +1,13 @@
 package a4everstudent.listviewdemo;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -17,22 +18,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView todoView = (ListView) findViewById(R.id.toDoListView);
+        ListView toDoView = (ListView) findViewById(R.id.toDOListView);
 
-        final ArrayList<String> toDo= new ArrayList<String>();
-        toDo.add("feed pets");
-        toDo.add("complete lecture 64");
-        toDo.add("finnish section 5");
+        final ArrayList<String>toDoList = new ArrayList<String>();
+        toDoList.add("feed pets");
+        toDoList.add("exercise");
+        toDoList.add("finnish book");
+        toDoList.add("call family");
+        toDoList.add("dinner @20h");
 
-        ArrayAdapter<String>arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, toDo);
+        ArrayAdapter<String> todoAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, toDoList);
+        toDoView.setAdapter(todoAdapter);
 
-        todoView.setAdapter(arrayAdapter);
-
-        todoView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
+        toDoView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Log.i("item tapped", toDo.get(position) );
+                Context context = getApplicationContext();
+                CharSequence text = toDoList.get(position);
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
             }
         });
 
